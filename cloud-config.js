c7465f90ec6e17774feb79d4a9f8a0d6111e5a93
George Lawn <georgelawn@Georges-MacBook-Air-2.local>
2026-06-03 11:25:40 +0100

    Configure Google Apps Script calendar sync

--- cloud-config.js
+++ cloud-config.js
@@ -3,5 +3,5 @@ window.ELLA_CLOUD_CONFIG = {
   supabaseUrl: "https://hmwnkhgsocdevehebjpq.supabase.co",
   supabaseAnonKey: "sb_publishable_5RLD78oK3TMo-SnPf1tljA_9d7-zuHn",
   tableName: "ella_crow_store",
-  googleCalendarSyncUrl: ""
+  googleCalendarSyncUrl: "https://script.google.com/macros/s/AKfycbwhJpMVQ1AQ0p5evufgLTmL2_Mnjk4P91WTPwHWYiD1nly69IyMc2V3WwowJRC-LJ8/exec"
 };
